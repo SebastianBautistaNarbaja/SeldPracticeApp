a48801fecf6817e20cf3a404116638e34f37788e
Sebastian <sebastianbautistanarbaja@gmail.com>
2026-03-20 17:56:36 +0100

    Create class DatabaseConnection

---
+++ src/dao/DatabaseConnection.java
@@ -0,0 +1,26 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DatabaseConnection {
+    private String url = "jdbc:mysql://localhost:3306/store";
+    private String user = "root";
+    private String password = "";
+    private Connection conexion ;
+
+    public DatabaseConnection(){
+       try {
+           conexion = DriverManager.getConnection(url,user,password);
+       } catch (SQLException e) {
+           //lanzo el error para que el main avise si hubo un error
+           throw new RuntimeException(e);
+       }
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+}
